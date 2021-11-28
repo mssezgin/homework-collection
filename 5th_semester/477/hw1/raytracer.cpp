b1f14feddf5236f1c1554ec73aa8ccfc292b8615
mssezgin <m.68sezgin@gmail.com>
2021-11-28 19:37:38 +0300

    create macro NUM_THREAD and change execution time calculations

--- 5th_semester/477/hw1/raytracer.cpp
+++ 5th_semester/477/hw1/raytracer.cpp
@@ -3,10 +3,11 @@
 #include "ppm.h"
 #include "models.h"
 
-#define IS_DEVELOPMENT true
+#define NUM_THREADS 4
+#define IS_DEVELOPMENT 1
 
 #if IS_DEVELOPMENT
-    #include <ctime>
+    #include <chrono>
 #endif
 
 
@@ -14,11 +15,8 @@ int main(int argc, char* argv[]) {
 
 
     #if IS_DEVELOPMENT
-        clock_t begin, end;
-        std::cout << "\n";
-        if ((begin = clock()) == -1)
-            std::cout << "\x1b[31mClock error occurred while calculating the total start time.\x1b[0m\n";
-        std::cout << argv[1] << "\n\n";
+        auto begin = std::chrono::high_resolution_clock::now();
+        std::cout << "\n" << argv[1] << "\n\n";
     #endif
 
 
@@ -29,26 +27,24 @@ int main(int argc, char* argv[]) {
         Camera &camera = *itr;
         size_t width = camera.nearPlane.imageWidth;
         size_t height = camera.nearPlane.imageHeight;
-        size_t heightBoundaries[5];
+        size_t heightBoundaries[NUM_THREADS + 1];
 
         heightBoundaries[0] = 0;
-        for (int i = 0, j = 4; i < 4; i++, j--) {
+        for (int i = 0, j = NUM_THREADS; i < NUM_THREADS; i++, j--) {
             heightBoundaries[i+1] = heightBoundaries[i] + (width - heightBoundaries[i]) / j;
         }
 
 
         #if IS_DEVELOPMENT
-            clock_t begin_camera, begin_write, end_camera;
+            auto begin_camera = std::chrono::high_resolution_clock::now();
             std::cout << "\t" << camera.nearPlane.imageName << " (" << width << "x" << height << ")\n";
-            if ((begin_camera = clock()) == -1)
-                std::cout << "\t\x1b[31mClock error occurred while calculating the camera start time.\x1b[0m\n";
         #endif
 
 
         std::vector<std::thread> threads;
         unsigned char *image = new unsigned char[width * height * 3];
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < NUM_THREADS; i++) {
             threads.push_back(std::thread(renderQuarter, image, width, heightBoundaries[i], heightBoundaries[i + 1], camera));
         }
 
@@ -58,9 +54,9 @@ int main(int argc, char* argv[]) {
 
 
         #if IS_DEVELOPMENT
-            if ((begin_write = clock()) == -1)
-                std::cout << "\t\x1b[31mClock error occurred while calculating the write start time.\x1b[0m\n";
-            std::cout << "\tRendered the image in " << (double) (begin_write - begin_camera) / CLOCKS_PER_SEC << " seconds\n";
+            auto begin_write = std::chrono::high_resolution_clock::now();
+            auto renderDuration = std::chrono::duration_cast<std::chrono::nanoseconds>(begin_write - begin_camera);
+            std::cout << "\tRendered the image in " << renderDuration.count() * 1e-9 << " seconds\n";
         #endif
 
 
@@ -69,17 +65,18 @@ int main(int argc, char* argv[]) {
 
 
         #if IS_DEVELOPMENT
-            if ((end_camera = clock()) == -1)
-                std::cout << "\t\x1b[31mClock error occurred while calculating the camera end time.\x1b[0m\n";
-            std::cout << "\tWrote to the file in " << (double) (end_camera - begin_write) / CLOCKS_PER_SEC << " seconds\n";
-            std::cout << "\t\x1b[32mCamera execution time: " << (double) (end_camera - begin_camera) / CLOCKS_PER_SEC << " seconds\x1b[0m\n\n";
+            auto end_camera = std::chrono::high_resolution_clock::now();
+            auto writeDuration = std::chrono::duration_cast<std::chrono::nanoseconds>(end_camera - begin_write);
+            auto cameraDuration = std::chrono::duration_cast<std::chrono::nanoseconds>(end_camera - begin_camera);
+            std::cout << "\tWrote to the file in " << writeDuration.count() * 1e-9 << " seconds\n";
+            std::cout << "\t\x1b[32mCamera execution time: " << cameraDuration.count() * 1e-9 << " seconds\x1b[0m\n\n";
         #endif
     }
 
 
     #if IS_DEVELOPMENT
-        if ((end = clock()) == -1)
-            std::cout << "\x1b[31mClock error occurred while calculating the total end time.\x1b[0m\n";
-        std::cout << "\x1b[42mTotal execution time: " << (double) (end - begin) / CLOCKS_PER_SEC << " seconds\x1b[0m\n" << std::endl;
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+        std::cout << "\x1b[42mTotal execution time: " << duration.count() * 1e-9 << " seconds\x1b[0m\n" << std::endl;
     #endif
 }
