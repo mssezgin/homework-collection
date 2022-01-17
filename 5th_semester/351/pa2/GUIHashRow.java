cf2a0d31d61a6f31324280f0d9fda1408d269542
mssezgin <m.68sezgin@gmail.com>
2022-01-17 20:38:28 +0300

    reformat code

--- 5th_semester/351/pa2/GUIHashRow.java
+++ 5th_semester/351/pa2/GUIHashRow.java
@@ -10,54 +10,46 @@ import javax.swing.border.TitledBorder;
 
 @SuppressWarnings("serial")
 public class GUIHashRow extends JPanel {
-	
-	private CengHashRow hashRow;
-	
-	public GUIHashRow(CengHashRow hashRow)
-	{
-		this.hashRow = hashRow;
-		
-		setupStyle();
-		setupWithRow();
-		
-		this.revalidate();
-		this.repaint();
-	}
 
-	private void setupStyle()
-	{		
-		if(this.hashRow.isVisited())
-		{
-			Border border = BorderFactory.createLineBorder(Color.GREEN, 2);
+    private CengHashRow hashRow;
 
-			this.setBorder(border);
-		}
-		else
-		{
-			Border border = BorderFactory.createLineBorder(Color.BLACK, 2);
-			
-			this.setBorder(border);
-		}
-		
-		this.setMinimumSize(new Dimension(200, 60));
-		this.setMaximumSize(new Dimension(200, 60));
-	}
-	
-	private void setupWithRow()
-	{
-		JLabel prefixLabel = new JLabel(this.hashRow.hashPrefix());
-		prefixLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
-		
-		this.add(prefixLabel);
-	}
-	
-	public CengBucket bucketForRow()
-	{
-		return this.hashRow.getBucket();
-	}
-	
-	public Boolean isVisited()
-	{
-		return this.hashRow.isVisited();
-	}
+    public GUIHashRow(CengHashRow hashRow) {
+        this.hashRow = hashRow;
+
+        setupStyle();
+        setupWithRow();
+
+        this.revalidate();
+        this.repaint();
+    }
+
+    private void setupStyle() {
+        if (this.hashRow.isVisited()) {
+            Border border = BorderFactory.createLineBorder(Color.GREEN, 2);
+
+            this.setBorder(border);
+        } else {
+            Border border = BorderFactory.createLineBorder(Color.BLACK, 2);
+
+            this.setBorder(border);
+        }
+
+        this.setMinimumSize(new Dimension(200, 60));
+        this.setMaximumSize(new Dimension(200, 60));
+    }
+
+    private void setupWithRow() {
+        JLabel prefixLabel = new JLabel(this.hashRow.hashPrefix());
+        prefixLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        this.add(prefixLabel);
+    }
+
+    public CengBucket bucketForRow() {
+        return this.hashRow.getBucket();
+    }
+
+    public Boolean isVisited() {
+        return this.hashRow.isVisited();
+    }
 }
